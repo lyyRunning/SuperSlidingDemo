59b6d524d09f06f7f1cf66ded15a812802dd9dec
luo3672 <1138191036@qq.com>
2020-07-30 16:21:28 +0800

    aa

--- app/src/main/java/com/sumansoul/superslidingdemo/MainActivity.java
+++ app/src/main/java/com/sumansoul/superslidingdemo/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         sspl = findViewById(R.id.sspl);
         Button tvMode = findViewById(R.id.tvMode);
         ImageView ivLeft = findViewById(R.id.ivLeft);
